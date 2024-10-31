4957cd72bf878fd4d18804a393957d07ab529967
connellr023 <connell.reffo@ucalgary.ca>
2024-10-30 19:56:58 -0600

    Some work

--- src/lib/ExperienceEntry.svelte
+++ src/lib/ExperienceEntry.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
+  import type { GraphData } from "$lib";
+  import Graph from "./Graph.svelte";
+
   export let title: string;
+  export let href: string;
   export let description: string;
   export let stack: string[];
   export let languages: [string, string][] = [];
+
+  let languageData = languages.map(([a, b]) => [
+    a,
+    b,
+    1,
+  ]) as unknown as GraphData[];
 </script>
+
+<a class="entry-container left-bar red" draggable={false} {href}>
+  <h3 class="entry-title">
+    <span class="token-green">{title}</span>
+  </h3>
+  <span class="arrow">{"->"}</span>
+</a>
+<p class="left-bar">{description}</p>
+<p class="left-bar entry-stack comment">{stack.join(", ")}</p>
+<Graph data={languageData} />
--- src/lib/ProjectEntry.svelte
+++ src/lib/ProjectEntry.svelte
@@ -10,45 +10,13 @@
   export let href: string = "";
 </script>
 
-<a class="container left-bar red" draggable={false} {href}>
-  <h3>
+<a class="entry-container left-bar red" draggable={false} {href}>
+  <h3 class="entry-title">
     <span class="token-red">{isPrivate ? "private" : "public"}</span>
     <span class="underline string">{name}</span>
   </h3>
   <span class="arrow">{"->"}</span>
 </a>
 <p class="left-bar">{description}</p>
-<p class="left-bar stack comment">{stack.join(", ")}</p>
+<p class="left-bar entry-stack comment">{stack.join(", ")}</p>
 <Graph data={languageData} />
-
-<style lang="scss">
-  @import "../styles/variables.scss";
-
-  h3 {
-    font-weight: normal;
-    margin: 0;
-    flex-grow: 1;
-    position: relative;
-  }
-
-  span.arrow {
-    color: $gruvbox-grey-2;
-    padding-right: 0.5rem;
-    transform: translateX(-0.5rem);
-    transition: all 0.15s ease;
-    font-size: 1.1rem;
-    white-space: nowrap;
-  }
-
-  a.container {
-    &:hover span.arrow {
-      color: $gruvbox-white;
-      transform: translateX(0);
-    }
-  }
-
-  p.stack {
-    padding-bottom: 0.3rem;
-    border-color: $gruvbox-grey-2;
-  }
-</style>
